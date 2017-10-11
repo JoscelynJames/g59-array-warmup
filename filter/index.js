40522fd407830a9bb3653793355c213c22d248cb
Joscelyn James <josce.james7@hotmail.com>
2017-10-11 13:54:14 -0600

    Some Passing

--- filter/index.js
+++ filter/index.js
@@ -1,20 +1,27 @@
 function onlyEven(array) {
-  const evens = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 === 0) {
-      evens.push(array[i]);
-    }
-  }
-  return evens;
+  return array.filter(num => num % 2 === 0);
+  // const evens = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] % 2 === 0) {
+  //     evens.push(array[i]);
+  //   }
+  // }
+  // return evens;
 }
 // refactor with filter!! OMG!
 
 function onlyOneWord(array) {
   //where are the snacks?
+  return array.filter((word) => {
+    console.log(word.length, '========')
+  })
 }
 
 function positiveRowsOnly(array) {
   //is Dan human?
+  array.filter((row) => {
+    row.indexOf() % 2 === 0 
+  })
 }
 
 module.exports = {
